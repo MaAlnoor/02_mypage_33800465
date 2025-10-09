43479d3ab15e8b0cd45376cf1ce9a897f7b0165e
Mohammed Alnoor <mohammed1169@hotmail.co.uk>
2025-10-09 21:16:45 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const http = require("http");
+const port = 8000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>My Page</title>
+      </head>
+      <body>
+        <h1>Welcome to My Page</h1>
+        <h2>About Me</h2>
+        <p>Hello my name is Mohammed Alnoor and I am a student.</p>
+      </body>
+    </html>
+  `);
+}).listen(port, () => {
+  console.log(`MyPage web app is running on http://localhost:${port}`);
+});
