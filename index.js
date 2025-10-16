6f2d9242d7da278da29b66b99e7df08d2c4b19a3
Mohammed Alnoor <mohammed1169@hotmail.co.uk>
2025-10-16 23:05:59 +0100

    added new line to test in vm

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ http.createServer((req, res) => {
         <h1>Welcome to My Page</h1>
         <h2>About Me</h2>
         <p>Hello my name is Mohammed Alnoor and I am a student.</p>
+        <p>Adding new line to test in VM.</p>
       </body>
     </html>
   `);
